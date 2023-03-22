2646c181a933e18d73794db8c5c84d185594aad7
Vivek Sharma <vs63961@gmail.com>
2023-03-22 19:22:31 +0530

    update

--- python/test/p9.py
+++ python/test/p9.py
@@ -2,20 +2,22 @@ class Complex:
     def __init__(self, real=0.0, imaginary=0.0):
         self.real = real
         self.imaginary = imaginary
-    
+        
     def __str__(self):
         return 'Sum of complex number: ' + str(self.real) + ' + ' + str(self.imaginary) + 'i'
 
     def __add__(self, other):
-        return Complex(self.real + other.real, self.imaginary + other.imaginary)
-    
-n = int(input("Enter the number of complex numbers to be added: "))
+        result_real = self.real + other.real
+        result_imaginary = self.imaginary + other.imaginary
+        return Complex(result_real, result_imaginary)
+
+n = int(input('Enter number of complex numbers to be added: '))
 c3 = Complex()
 for i in range(n):
-    print(f"Enter Complex number {i+1}:")
-    r = float(input("Enter real: "))
-    i = float(input("Enter imaginary: "))
-    c2 = Complex(r, i)
+    print('Enter Complex number', i+1)
+    r = float(input('Enter real part: '))
+    j = float(input('Enter imaginary part: '))
+    c2 = Complex(r, j)
     c3 = c3 + c2
 
 print(c3)
